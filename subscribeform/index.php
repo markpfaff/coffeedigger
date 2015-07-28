33005419738d35227527d793e6a48fe2460a80ae
Luis Gamboa <luisgamboasierra@gmail.com>
2015-07-27 18:18:03 -0700

    i created a subscribe form

---
+++ subscribeform/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+	<script src="js/modernizr.js"></script> <!-- Modernizr -->
+    
+<title>Subscribe Box</title>
+</head>
+
+<body>
+
+<!-- Subscription Form -->
+<form action="form.php" method="post">
+    <input name="email" class="email" type="text" placeholder="Enter your email address ...">
+    <button type="submit" class="btn_email">Send</button>
+</form>
+<!-- End Subscription Form -->
+
+
+
+
+
+<script src="js/jquery-2.1.1.js"></script>
+<script src="js/main.js"></script> <!-- Resource jQuery -->
+
+</body>
+</html>
