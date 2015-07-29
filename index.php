f91c0845f77d26ec15e7cf25f8a52664dfb8f509
markpfaff <markdpfaff@gmail.com>
2015-07-28 18:22:43 -0700

    Switches to new index.php

--- index.php
+++ index.php
@@ -1,241 +1,389 @@
-<?php
-//coffeedigger home page
-include('includes/config.php');
-?>	
 <!DOCTYPE html>
 <html>
 
 <head>
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-          <link type="text/css" rel="stylesheet" href="css/style.css">
-        <link type="text/css" rel="stylesheet" href="css/example.css">
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-      
-    <style>
-        header {
-            background-color:#dccccc;
-            color:white;
-            text-align:center;
-            padding:5px;	 
-        }
-        nav {
-            line-height:30px;
-            background-color:#eeeeee;
-            height:360px;
-            width:120px;
-            float:left;
-            padding:5px;	      
-        }
-        section {
-            width:350px;
-            float:left;
-            padding:10px;	 	 
-        }
-        footer {
-            background-color:black;
-            color:white;
-            clear:both;
-            text-align:center;
-            padding:5px;	 	 
-        }
-        ul{
-            list-style-type:none;
-
-        }
-
-        /*unvisited link*/
-        a{text-decoration:none;}
-        a  {
-            color:#003;
-        }
-        /*visited link*/
-        a {
-            color:#999;
-
-        }
-        /*mouse over link*/
-        a:hover{
-            color:#FF0;
-        ;
-        }
-        /*selected link*/
-        a:active{
-            color:#F33;
-
-        }
-    </style>
+	<title>CoffeeDigger Main Page</title>
+	
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	
+    <link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    
 </head>
 
-    <body>
-        <!--header -->
-        <header>
-        <div class="logo"><a href="index.html"><img class="icon" alt="" src="images/coffeedigger2.png" title="link back to main page"></a></div>
-        </header>
+<body>
+<!--header -->
+<header>
 
-        <!--navigation -->
-        <nav>
-        <ul>
-            <li class ='active'><a href="#">Home</a></li>
-            <li class ='active'><a href="#">Groups</a></li>
-            <li class ='active'><a href="#">Contact Us</a></li>
-         </ul>
-
-        </nav>
-
-        <!--section -->
-        <section>
-            <h1>HA HA</h1>
-            <img src ="images/kangaroo.jpg" alt=""/>
-            <hr />
-            <h2>Dig it!</h2>
-            <form class="form-horizontal" id="form_members" role="form" action="send_formdata_coffees.php" method="POST">
-                    <h3>Awesome Coffee</h3>
-
-                    <h4>
-                        <?php 
-                        while($row = mysqli_fetch_array($result))
-                        {
-                        echo $row['digs'];
-                        }
-                        mysqli_close($con);
-                        ?> Digs!
-                    </h4>    
-
-                        <input type="submit" class="form-control" name="digs" value="Dig It!">
-
-            </form>
-            <hr />
-            <h2>Comments!</h2>
-            <div class="cmt-container" >
-                <?php
-		$id_post = "1"; //the post or the page id; 
-                $sql = mysql_query("SELECT * FROM comment WHERE id_post = '$id_post'") or die(mysql_error());;
-                while($affcom = mysql_fetch_assoc($sql)){ 
-                    $name = $affcom['name'];
-                    $email = $affcom['email'];
-                    $comment = $affcom['comment'];
-                    $date = $affcom['date'];
-
-                    // Get gravatar Image 
-                    // https://fr.gravatar.com/site/implement/images/php/
-                    $default = "mm";
-                    $size = 35;
-                    $grav_url = "http://www.gravatar.com/avatar/".md5(strtolower(trim($email)))."?d=".$default."&s=".$size;
-
-                ?>
-                <div class="cmt-cnt">
-                    <img src="<?php echo $grav_url; ?>" />
-                    <div class="thecom">
-                        <h5><?php echo $name; ?></h5><span data-utime="1371248446" class="com-dt"><?php echo $date; ?></span>
-                        <br/>
-                        <p>
-                            <?php echo $comment; ?>
-                        </p>
-                    </div>
-                </div><!-- end "cmt-cnt" -->
-                <?php } ?>
+<div class="container">
+<div class="col-md-4">
+<nav>
+<ul>
+	<li><a href="about.html">About</a></li>
+	<li><a href="contact.html">Contact</a></li>
+</ul>
+</nav>
+</div>    
+    
+<div class="logo col-md-4">
+    
+        <a href="index.html"><img class="icon" alt="logo" src="images/logo.png" title="link back to main page"></a>
+</div>
 
 
-                <div class="new-com-bt">
-                    <span>Write a comment ...</span>
-                </div>
-                <div class="new-com-cnt">
-                    <input type="text" id="name-com" name="name-com" value="" placeholder="Your name" />
-                    <input type="text" id="mail-com" name="mail-com" value="" placeholder="Your e-mail adress" />
-                    <textarea class="the-new-com"></textarea>
-                    <div class="bt-add-com">Post comment</div>
-                    <div class="bt-cancel-com">Cancel</div>
-                </div>
-                <div class="clear"></div>
-            </div><!-- end of comments container "cmt-container" -->
-            <div>
-              <h2>Search Box!</h2>
-              <form action="" method="get">
-                  <p>
-                      <input type="text" name="term"/>
-                      <input type="submit" value="search"/>
-                  </p>
-              </form>
-            </div>
+<div class="col-md-4">
 
-            <div>
+<div class="from-group">
 
-                <?php
-                    if (empty($_GET['term']) === false){
-                        $search_results = search_posts($_GET['term']);
-
-                        if (empty($search_results)){
-                            echo 'Your search returned no results.';
-                            }
-
-                            foreach ($search_results as $result){
-                                echo "<h3>Results:</h3>";
-                                echo "<h4>{$result['title']}</h4>";
-                                }
-                        }
-                ?>
-
-            </div>
+<form class="navbar-form searchbox" role="search">
+                     <div class="input-group">
+                          <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+                            <div class="input-group-btn">
+                                 <button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+                           </div>
+                     </div>
+                   </form>
         
-        </section>
+</div>                   
+</div>    
 
-        <script type="text/javascript">
-           $(function(){ 
-                //alert(event.timeStamp);
-                $('.new-com-bt').click(function(event){    
-                    $(this).hide();
-                    $('.new-com-cnt').show();
-                    $('#name-com').focus();
-                });
+</div>    
+</header>
 
-                /* when start writing the comment activate the "add" button */
-                $('.the-new-com').bind('input propertychange', function() {
-                   $(".bt-add-com").css({opacity:0.6});
-                   var checklength = $(this).val().length;
-                   if(checklength){ $(".bt-add-com").css({opacity:1}); }
-                });
 
-                /* on clic  on the cancel button */
-                $('.bt-cancel-com').click(function(){
-                    $('.the-new-com').val('');
-                    $('.new-com-cnt').fadeOut('fast', function(){
-                        $('.new-com-bt').fadeIn('fast');
-                    });
-                });
+    
+    
+<div class="form-group filter">
 
-                // on post comment click 
-                $('.bt-add-com').click(function(){
-                    var theCom = $('.the-new-com');
-                    var theName = $('#name-com');
-                    var theMail = $('#mail-com');
+<label for="filter">Sort By:</label>
+<select class="form-control">
+  <option value="">Brand</option>
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="opel">Opel</option>
+  <option value="audi">Audi</option>
+</select>
+  
+<select class="form-control">
+  <option value="">Type</option>
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="opel">Opel</option>
+  <option value="audi">Audi</option>
+</select>    
+ 
+<select class="form-control">
+  <option value="">Flavor</option>
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="opel">Opel</option>
+  <option value="audi">Audi</option>
+</select>   
 
-                    if( !theCom.val()){ 
-                        alert('You need to write a comment!'); 
-                    }else{ 
-                        $.ajax({
-                            type: "POST",
-                            url: "includes/add-comment.php",
-                            data: 'act=add-com&id_post='+<?php echo $id_post; ?>+'&name='+theName.val()+'&email='+theMail.val()+'&comment='+theCom.val(),
-                            success: function(html){
-                                theCom.val('');
-                                theMail.val('');
-                                theName.val('');
-                                $('.new-com-cnt').hide('fast', function(){
-                                    $('.new-com-bt').show('fast');
-                                    $('.new-com-bt').before(html);  
-                                })
-                            }  
-                        });
-                    }
-                });
+<select class="form-control">
+  <option value="">Price</option>
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="opel">Opel</option>
+  <option value="audi">Audi</option>
+</select>
+    
+<select class="form-control">
+  <option value="">Popularity</option>
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="opel">Opel</option>
+  <option value="audi">Audi</option>
+</select>    
 
-            });
-        </script>
+<button class="btn btn-default filtergo" type="submit">Go</button>
 
-        <!--footer -->
-        <footer>
-        Copyright &copy; kangaroo 
-        </footer>
 
-    </body>
+        
+</div>
+
+
+
+<!--section -->
+<section>
+<!--Container for grid-->
+<div class="container">
+
+<!--Images for groups-->
+	<div class="mainbean">
+
+        
+        <div class="row">
+			<div class="col-md-3">
+                <div class="overview">
+                
+                <div class="review">
+                <h3>Coffee1</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                    
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+            <div class="col-md-3">
+                <div class="overview">
+				
+                <div class="review">
+                <h3>Coffee2</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+            <div class="col-md-3">
+                <div class="overview">
+				
+                <div class="review">
+                <h3>Coffee3</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                    
+                    
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+            <div class="col-md-3">
+                <div class="overview">
+				
+                <div class="review">
+                <h3>Coffee4</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                    
+                    
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+		</div> <!--End container for Row-->
+        
+        <div class="row">
+			<div class="col-md-3">
+                <div class="overview">
+                
+                <div class="review">
+                <h3>Coffee5</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                    
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+            <div class="col-md-3">
+                <div class="overview">
+				
+                <div class="review">
+                <h3>Coffee6</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+            <div class="col-md-3">
+                <div class="overview">
+				
+                <div class="review">
+                <h3>Coffee7</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                    
+                    
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+            <div class="col-md-3">
+                <div class="overview">
+				
+                <div class="review">
+                <h3>Coffee8</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                    
+                    
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+		</div> <!--End container for Row-->
+        
+        <div class="row">
+			<div class="col-md-3">
+                <div class="overview">
+                
+                <div class="review">
+                <h3>Coffee9</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                    
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+            <div class="col-md-3">
+                <div class="overview">
+				
+                <div class="review">
+                <h3>Coffee10</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+            <div class="col-md-3">
+                <div class="overview">
+				
+                <div class="review">
+                <h3>Coffee11</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                    
+                    
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+            <div class="col-md-3">
+                <div class="overview">
+				
+                <div class="review">
+                <h3>Coffee12</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                    
+                    
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+		</div> <!--End container for Row-->
+        
+           <div class="row">
+			<div class="col-md-3">
+                <div class="overview">
+                
+                <div class="review">
+                <h3>Coffee13</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                    
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+            <div class="col-md-3">
+                <div class="overview">
+				
+                <div class="review">
+                <h3>Coffee14</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+            <div class="col-md-3">
+                <div class="overview">
+				
+                <div class="review">
+                <h3>Coffee15</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                    
+                    
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+            <div class="col-md-3">
+                <div class="overview">
+				
+                <div class="review">
+                <h3>Coffee16</h3>
+                <img src="images/shovel.png" alt="shovel" >
+                <p>123</p>
+                </div>
+                    
+                    
+                <hr>
+				<img src="images/bean1.jpg" class="img-rounded" alt="images for shops" >
+                </div>
+			</div>
+			
+		</div> <!--End container for Row-->
+
+        
+	</div> <!--End container for images-->
+</div> <!--End container-->
+
+</section>
+
+<!--footer -->
+<footer class="foooter">
+<div class="container">
+    <div class="col-md-4 copy"><p>Copyright &copy;2015</p></div> 
+    
+        <div class="col-md-4 social"> 
+
+<a href="#" class="btn btn-social-icon btn-instagram"> <i class="fa fa-instagram"></i></a>
+
+<a href="#" class="btn btn-social-icon btn-facebook"> <i class="fa fa-facebook"></i></a>
+
+</div>
+    
+    <div class="col-md-4"><p>Webdesign by Kangaroo</p></div>
+
+</footer>
+
+</body>
 </html>
+
