dbc3560907b56a03ea3fa0318ec3851d55748982
cluetrain <greenstrat67@gmail.com>
2015-08-06 18:17:08 -0700

    contact

---
+++ demos/website/aside.php
@@ -0,0 +1,35 @@
+<!--BEGINS SIDEBAR-->
+<div id="right-sidebar" class="sidebar">
+    <aside class="inner-aside">
+    		<h2>LATEST Bean!</h2>
+            <img src="img/grains-coffee1.jpg" class="aside-box" alt="user-image" >
+
+	</aside>
+    
+    <aside class="inner-aside">
+        <h2>POPULAR Bean!</h2>
+        <img src="img/grains-coffee2.jpg" class="aside-box" alt="user-image" >
+      
+    </aside>
+    
+    <aside class="inner-aside-2">
+    <h2>Newsletter Sign-up</h2>
+    
+            <form action="form.php" id="form-style" method="post"> 
+        	<div>
+                <label class="newsletter-1">
+                	<input type="text" id="name-sub" name="name" value="" placeholder="Name">
+                </label>
+                
+                <label class="newsletter-1">
+                	<input type="text" id="name-sub" name="email" value="" placeholder="E-mail">
+                </label>
+                
+                <button type="submit" id="submit-sub" value="Submit">Submit</button>
+        	</div>
+		</form> 
+
+    </aside>
+    
+    </div>
+<!--ENDS SIDEBAR-->
