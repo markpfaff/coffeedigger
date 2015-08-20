df930278aa3b5b9de81e0d4d38c8d367fbfe8524
markdavid <markdpfaff@gmail.com>
2015-08-20 12:49:53 -0700

    Dig upvote fix

--- BrazilCarmodeMinas-profile.php
+++ BrazilCarmodeMinas-profile.php
@@ -113,19 +113,20 @@ $title = 'Coffeedigger | Profile ';
             <div class="profile-overview">
                     <div class="profile-review">
                         <h3>Brazil Carmo de Minas</h3>
-                        <img src="images/shovel.png" alt="shovel" >
                         
                         <form id="form_members" role="form" action="send_formdata_coffees.php" method="POST">
                         	   <button class="dig" type="submit" name="digs" value="Submit" >
-                                   <img class="button-dig" src="images/shovel.png" alt="Submit">Dig
+                                   <img class="button-dig" src="images/shovel.png" alt="Submit">Dig <span class="dig-score">
+                                        <?php
+                                        $sql = mysql_query("SELECT * FROM coffee WHERE ID = '7'") or die(mysql_error());
+                                        while($row = mysql_fetch_assoc($sql)){
+                                             echo $row['digs'];
+                                                }
+                                        ?>   
+                                   </span>
                                </button>
                         </form>                            
-                        <?php
-                        $sql = mysql_query("SELECT * FROM coffee WHERE ID = '7'") or die(mysql_error());
-                        while($row = mysql_fetch_assoc($sql)){
-                             echo $row['digs'];
-                                }
-                        ?>                                 
+                              
                        
                     </div>
                  
